d20a969cc4c9ec9763abcc9bc36b70b160a93c73
shubhsaini03 <122141824+shubhsaini03@users.noreply.github.com>
2024-07-17 16:02:12 +0530

    in

---
+++ LibaryManage/LibaryManage/Program.cs
@@ -0,0 +1,37 @@
+
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DB"));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("myCorsPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("myCorsPolicy");
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
